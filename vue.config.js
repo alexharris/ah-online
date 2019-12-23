05fedb97430f6f5e3e57ad3c49c5f19a2f0a1b9e
alexharris <alexharris6@gmail.com>
2019-12-22 22:57:38 -0800

    url fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production'
-    //   ? '/alma/'
-    //   : '/'
-    publicPath: '/',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/ah-online/'
+      : '/',
+    // publicPath: '/',
     outputDir: "docs"
   }

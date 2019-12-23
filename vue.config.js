8396a8439ff6cfbe5fbb0909901527ce630bf31b
alexharris <alexharris6@gmail.com>
2019-12-22 23:07:14 -0800

    remove baseurl

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/ah-online/'
-      : '/',
-    // publicPath: '/',
+    // publicPath: process.env.NODE_ENV === 'production'
+    //   ? '/alma/'
+    //   : '/'
+    publicPath: '/',
     outputDir: "docs"
   }

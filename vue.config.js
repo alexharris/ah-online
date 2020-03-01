b7d9e0471aa8fdfb9507942e75f4ae311ba0d179
alexharris <alexharris6@gmail.com>
2020-02-29 18:02:32 -0800

    more form tests

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,23 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production'
-    //   ? '/alma/'
-    //   : '/'
     publicPath: '/',
-    outputDir: "docs"
+    outputDir: "docs",
+    configureWebpack: () => {
+      if (process.env.NODE_ENV !== 'production') return;
+      return {
+        plugins: [
+          new PrerenderSPAPlugin(
+            // Absolute path to compiled SPA
+            path.resolve(__dirname, 'dist'),
+            // List of routes to prerender
+            [ '/'],
+            {
+              // options
+            }
+          ),
+        ]
+      }
+    }    
   }
